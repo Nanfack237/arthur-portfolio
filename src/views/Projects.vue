f58e65b2e671a44069f77ff6ade2eb22798d956c
Donald NKENGFACK <donald.nkengfack@sunshine.mx>
2025-08-02 23:44:08 +0100

    Update projetc

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -6,7 +6,7 @@
         title="E-commerce Platform Redesign"
         description="Redesigned and developed a modern e-commerce platform using HTML, CSS, JavaScript, and PHP. Focused on user experience and responsive design. This involved Adobe XD for UI/UX prototyping and Adobe Photoshop for asset creation."
         tools="HTML, CSS, JavaScript, PHP, MySQL, Adobe XD, Adobe Photoshop"
-        imageSrc="public/images/ecommerce1.jpeg"
+        imageSrc="/public/images/ecommerce1.jpeg"
         liveLink="#"
         githubLink="#"
       />
@@ -14,7 +14,7 @@
         title="Animated Explainer Video Series"
         description="Produced a series of animated explainer videos for a client's new product launch. Involved scriptwriting, storyboarding, character design, animation, and sound design."
         tools="Adobe Premiere Pro, Adobe After Effects, Illustrator"
-        imageSrc="public/images/seriesplanner1.jpeg"
+        imageSrc="/public/images/seriesplanner1.jpeg"
         liveLink="#"
         githubLink="#"
       />
@@ -22,7 +22,7 @@
         title="Brand Identity & Marketing Collateral"
         description="Developed a complete brand identity package for a startup, including logo design, color palette, typography, and various marketing materials like brochures and social media graphics. Leveraged Figma for collaborative design."
         tools="Adobe Illustrator, Adobe InDesign, Figma, Canva"
-        imageSrc="public/images/marketing1.jpeg"
+        imageSrc="/public/images/marketing1.jpeg"
         liveLink="#"
         githubLink="#"
       />
@@ -30,7 +30,7 @@
          title="YouTube Video Management & Monetization"
          description="Managed content, edited videos, handled publication, and optimized for monetization for a YouTube channel. Focused on audience engagement and growth strategies. (From Edmonde Association work)"
          tools="Filmora, YouTube Studio"
-         imageSrc="public/images/youtube.jpeg"
+         imageSrc="/public/images/youtube.jpeg"
          liveLink="https://www.youtube.com/your-channel-link"
          githubLink=""
        />
